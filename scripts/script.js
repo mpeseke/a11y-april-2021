1beeaa051a7779116dbf0201b6ca47325e7e7bec
Marlon Oliver Peseke <mpeseke@gmail.com>
2021-04-13 16:04:33 -0600

    main project dump
    includes everything...

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+
+// let sum = 0; 
+// let min = 1;
+// let max = 31;
+
+// for (i = min; i < max; i++) {
+//     if(i % 2 == 1) {
+//         sum += i;
+//     }   
+// }
+
+// console.log(sum);
